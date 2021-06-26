da21fafab549936c24345bc07187529634058cec
g-petra <83452177+g-petra@users.noreply.github.com>
2021-06-26 16:39:18 +0200

    Add files via upload

---
+++ Movies/12.sql
@@ -0,0 +1,11 @@
+SELECT movies.title
+FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE people.name = "Johnny Depp"
+  AND movies.title IN
+    (SELECT movies.title
+     FROM movies
+     JOIN stars ON movies.id = stars.movie_id
+     JOIN people ON stars.person_id = people.id
+     WHERE people.name = "Helena Bonham Carter");
---
+++ Movies/7.sql
@@ -0,0 +1,7 @@
+SELECT title,
+       rating
+FROM ratings
+LEFT JOIN movies ON ratings.movie_id=movies.id
+WHERE year = 2010
+ORDER BY rating DESC,
+         title;
